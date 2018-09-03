80f34039c1d87a7166e33c812891a12badbded87
ZhouYuan <30629886+GrantWD@users.noreply.github.com>
2018-09-03 22:29:15 +0800

    header部分

--- music-webapp/src/main.js
+++ music-webapp/src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入了公共的样式
+import '@/assets/css/reset.css'
 
 Vue.config.productionTip = false
 
@@ -10,6 +12,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  components: { 
+    App
+  },
+  template: '<App></App>'
 })
