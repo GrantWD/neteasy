633a14d24b473239f966a888f81f5f3e3d44cbdb
ZhouYuan <30629886+GrantWD@users.noreply.github.com>
2018-09-04 16:52:31 +0800

    歌单页面

--- music-webapp/src/App.vue
+++ music-webapp/src/App.vue
@@ -1,35 +1,26 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <Account></Account>
-=======
-    <Footer></Footer>
->>>>>>> 6120f84c2ba49ff0befefd1755ce01a265297a44
+   <MusicList></MusicList>
+
   </div>
 </template>
 <script>
-<<<<<<< HEAD
-  import Account from './page/Account'
-export default {
-  name: 'App',
-  components:{
-    Account,
-=======
-import Footer from './components/Footer.vue';
+import MusicList from './page/MusicList.vue';
 export default {
   name: 'App',
   data () {
     return {
-
     }
   },
   components: {
-    Footer
->>>>>>> 6120f84c2ba49ff0befefd1755ce01a265297a44
+    MusicList
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+	#app{
+		height: 100%;
+	}
+	
 </style>
