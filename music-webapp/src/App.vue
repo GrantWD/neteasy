7a7a6032dd67ed5f94c3f922cbe5b78bcdd4ac3f
Yang <857254562@qq.com>
2018-09-05 22:13:40 +0800

    周三fy

--- music-webapp/src/App.vue
+++ music-webapp/src/App.vue
@@ -1,48 +1,21 @@
 <template>
-<<<<<<< HEAD
-  <HostStation></HostStation>
-</template>
-
-<script>
-import HostStation from './page/HostStation';
-export default {
-  name: 'app',
-  components: {
-    HostStation
-=======
-<<<<<<< HEAD
   <div id="app">
-    <PlayerMusic></PlayerMusic>
-    <!-- <MusicList></MusicList> -->
+    <router-view></router-view>
   </div>
-=======
-  <HelloWorld></HelloWorld>
->>>>>>> 21d2a0c04b33f893d0da50f73facc2a40073966d
 </template>
 
 <script>
-<<<<<<< HEAD
-import PlayerMusic from './page/PlayerMusic.vue';
-import MusicList from './page/MusicList.vue';
-=======
-import HelloWorld from './page/HelloWorld';
->>>>>>> 21d2a0c04b33f893d0da50f73facc2a40073966d
-export default {
-  name: 'app',
-  components: {
-<<<<<<< HEAD
-    PlayerMusic,
-    MusicList
-  } 
-=======
-    HelloWorld
->>>>>>> 447f61bad83b4cfd0b05296ba3cc3e057915358d
+  import StyleRecommend from './page/StyleRecommend'
+  export default {
+    name: 'App',
+    components:{
+      StyleRecommend,
+    }
   }
->>>>>>> 21d2a0c04b33f893d0da50f73facc2a40073966d
-}
 </script>
-<style scoped lang="scss">
-  #app {
+
+<style scoped>
+  #app{
     height: 100%;
   }
 </style>
