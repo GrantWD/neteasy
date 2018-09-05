e9ac3eb3364f3abad3e847df8ce8a58e7a26b7e9
zhouyuan <xzy58188@163.com>
2018-09-05 22:33:06 +0830

    表单

--- music-admin/src/App.vue
+++ music-admin/src/App.vue
@@ -15,6 +15,7 @@ export default {
 </script>
 
 <style>
-#app {
+#app{
+  height: 100%;
 }
 </style>
---
+++ music-admin/src/components/Operate.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class='contentOperate'>
+        <a href="javascript:void(0)" class='add'><i class="iconfont icon-add"></i>添加歌曲</a>
+        <a href="javascript:void(0)" class='desc'><i class="iconfont icon-delete"></i>批量删除</a>
+        <a href="javascript:void(0)" class='deleteMulti'><i class="iconfont icon-share"></i>批量导出</a>
+        <a href="javascript:void(0)" class='exportMulti'><i class="iconfont icon-calendar"></i>详细信息</a>
+        <a href="javascript:void(0)" class='musicTotal'><i class="iconfont icon-folder"></i>总共数据<strong class="num">125</strong></a>
+    </div>
+</template>
+<script>
+export default {
+  name: 'contentOperate',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang='scss' scoped>
+    .contentOperate{
+        $mainColor:#ea2000;
+        font-size: 0;
+        a{
+            display: inline-block;
+            padding: 5px 8px;
+            margin-right: 5px;
+            background-color: $mainColor;
+            font-size: 14px;
+            color:#fff;
+            border-radius: 3px;
+            // border: 1px solid #ff2000;
+            .iconfont{
+                margin-right:4px;
+            }
+        }
+        
+    }
+</style>
