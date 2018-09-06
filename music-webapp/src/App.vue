0394af4bcdc6ae6bee5060ee846dedb72293a9fc
why <495893392@qq.com>
2018-09-06 10:54:21 +0800

    4w

--- music-webapp/src/App.vue
+++ music-webapp/src/App.vue
@@ -1,36 +1,19 @@
 <template>
-<<<<<<< HEAD
-  <Account></Account>
+  <video-reconmmend></video-reconmmend>
 </template>
 
 <script type="text/ecmascript-6">
-  import Account from './page/Account.vue'
+  import VideoReconmmend from "./page/VideoRecommend.vue";
+
+
   export default {
     components: {
-      Account
-    }
+      VideoReconmmend
+    },
   }
+
 </script>
 
-=======
-	<MusicList></MusicList>
-  <!-- <PlayerMusic></PlayerMusic> -->
-</template>
-
-<script>
-import MusicList from './page/MusicList';
-import PlayerMusic from './page/PlayerMusic';
-export default {
-  name: 'app',
-  components: {
-    MusicList,
-    PlayerMusic
-  }
-}
-</script>
-
-
->>>>>>> a36cbbde54f1598a5196478702b9f34cd1218505
-<style scoped lang="scss">
+<style lang="scss" scoped>
 
 </style>
