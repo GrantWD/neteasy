5d90ba3f1445672fecdbd3ab4f9959baffb03740
zhouyuan <xzy58188@163.com>
2018-09-07 20:07:40 +0830

    service

---
+++ music-service/musicapi/routes/radioclass.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var PATH = './public/data/'; // 配置文件的路径
+
+// 所有歌单数据
+router.get('/', function (req, res, next) {
+    var type = req.param('type') || ''; //获取url传递的参数，没有传递参数为空
+    // 读取本地文件data中的
+    fs.readFile(`${ PATH }radioclass.json`, function (err, data) {
+        if (err) {
+            return res.send({
+                status: 0,
+                info: '读取文件异常',
+                log: '为什么出错'
+            });
+        }
+        var obj = [];
+        // 异常处理，如果文件中保存的不是json格式的文件
+        try {
+            obj = JSON.parse(data.toString());
+        } catch (e) {
+            obj = [];
+        }
+        return res.send({
+            status: 1,
+            obj
+        });
+    })
+});
+
+module.exports = router;
--- music-webapp/src/App.vue
+++ music-webapp/src/App.vue
@@ -1,42 +1,31 @@
 <template>
-<<<<<<< HEAD
-  <MusicList></MusicList>
+  <MusicList :musicListInfo='musicListInfo'></MusicList>
 </template>
 <script>
 import MusicList from './page/MusicList';
 export default {
   name: 'app',
+  data () {
+    return {
+      musicListInfo:{}
+    }
+  },
   components: {
     MusicList,
+  },
+  created () {
+    $.get('http://localhost:3000/test', function(data) {
+		  console.log(data);
+		  console.log(data.status)
+		  this.musicListInfo=data.obj.musicListInfo
+		  console.log(this.musicListInfo);
+		});
   }
 }
 </script>
 
 <style scoped lang="scss">
-
   #app{
     height: 100%;
   }
-
-=======
-  <div id="app">
-    <router-view></router-view>
-  </div>
-</template>
-
-<script>
-  import RadioDetail from './page/RadioDetail'
-  export default {
-    name: 'App',
-    components:{
-      RadioDetail
-    }
-  }
-</script>
-
-<style scoped>
-  #app{
-    height: 100%;
-  }
->>>>>>> 9cb83ac2f1f8457850e3656e932d5f8d9c4ada00
 </style>
